6f7df0ba90eff71f0d41991da945b3407d0b8d32
Emanuel Komínek <drake127@planescape.cz>
2020-09-15 20:00:51 +0200

    Reintroduced optimizations.
    These optimizations work for both original and endsley's formats.

--- bsdiff.c
+++ bsdiff.c
@@ -222,7 +222,8 @@ static int bsdiff_internal(const struct bsdiff_request req)
 {
 	int64_t *I,*V;
 	int64_t scan,pos,len;
-	int64_t lastscan,lastpos,lastoffset;
+	int64_t lastscan,lastpos,lastoffset,lastwrittenscan,lastwrittenpos;
+	int64_t ctrlcur[3], ctrlnext[3];
 	int64_t oldscore,scsc;
 	int64_t s,Sf,lenf,Sb,lenb;
 	int64_t overlap,Ss,lens;
@@ -240,7 +241,8 @@ static int bsdiff_internal(const struct bsdiff_request req)
 
 	/* Compute the differences, writing ctrl as we go */
 	scan=0;len=0;pos=0;
-	lastscan=0;lastpos=0;lastoffset=0;
+	lastscan=0;lastpos=0;lastoffset=lastwrittenscan=lastwrittenpos=0;
+	ctrlcur[0]=ctrlcur[1]=ctrlcur[2]=0;
 	while(scan<req.newsize) {
 		oldscore=0;
 
@@ -293,30 +295,69 @@ static int bsdiff_internal(const struct bsdiff_request req)
 				lenb-=lens;
 			};
 
-			offtout(lenf,buf);
-			offtout((scan-lenb)-(lastscan+lenf),buf+8);
-			offtout((pos-lenb)-(lastpos+lenf),buf+16);
+			ctrlnext[0]=lenf;
+			ctrlnext[1]=(scan-lenb)-(lastscan+lenf);
+			ctrlnext[2]=(pos-lenb)-(lastpos+lenf);
 
-			/* Write control data */
-			if (writedata(req.stream, buf, sizeof(buf), BSDIFF_WRITECONTROL))
-				return -1;
+			if (ctrlnext[0]) {
+				if (ctrlcur[0]||ctrlcur[1]||ctrlcur[2]) {
+					offtout(ctrlcur[0],buf);
+					offtout(ctrlcur[1],buf+8);
+					offtout(ctrlcur[2],buf+16);
 
-			/* Write diff data */
-			for(i=0;i<lenf;i++)
-				buffer[i]=req.new[lastscan+i]-req.old[lastpos+i];
-			if (writedata(req.stream, buffer, lenf, BSDIFF_WRITEDIFF))
-				return -1;
+					/* Write control data */
+					if (writedata(req.stream, buf, sizeof(buf), BSDIFF_WRITECONTROL))
+						return -1;
 
-			/* Write extra data */
-			for(i=0;i<(scan-lenb)-(lastscan+lenf);i++)
-				buffer[i]=req.new[lastscan+lenf+i];
-			if (writedata(req.stream, buffer, (scan-lenb)-(lastscan+lenf), BSDIFF_WRITEEXTRA))
-				return -1;
+					/* Write diff data */
+					for(i=0;i<ctrlcur[0];i++)
+						buffer[i]=req.new[lastwrittenscan+i]-req.old[lastwrittenpos+i];
+					if (writedata(req.stream, buffer, ctrlcur[0], BSDIFF_WRITEDIFF))
+						return -1;
+
+					/* Write extra data */
+					for(i=0;i<ctrlcur[1];i++)
+						buffer[i]=req.new[lastwrittenscan+ctrlcur[0]+i];
+					if (writedata(req.stream, buffer, ctrlcur[1], BSDIFF_WRITEEXTRA))
+						return -1;
+
+					lastwrittenscan=lastscan;
+					lastwrittenpos=lastpos;
+				};
+				ctrlcur[0]=ctrlnext[0];
+				ctrlcur[1]=ctrlnext[1];
+				ctrlcur[2]=ctrlnext[2];
+			} else {
+				ctrlcur[1]+=ctrlnext[1];
+				ctrlcur[2]+=ctrlnext[2];
+			};
 
 			lastscan=scan-lenb;
 			lastpos=pos-lenb;
 			lastoffset=pos-scan;
 		};
+	};
+
+	if (ctrlcur[0]||ctrlcur[1]) {
+		offtout(ctrlcur[0],buf);
+		offtout(ctrlcur[1],buf+8);
+		offtout(ctrlcur[2],buf+16);
+
+		/* Write control data */
+		if (writedata(req.stream, buf, sizeof(buf), BSDIFF_WRITECONTROL))
+			return -1;
+
+		/* Write diff data */
+		for(i=0;i<ctrlcur[0];i++)
+			buffer[i]=req.new[lastwrittenscan+i]-req.old[lastwrittenpos+i];
+		if (writedata(req.stream, buffer, ctrlcur[0], BSDIFF_WRITEDIFF))
+			return -1;
+
+		/* Write extra data */
+		for(i=0;i<ctrlcur[1];i++)
+			buffer[i]=req.new[lastwrittenscan+ctrlcur[0]+i];
+		if (writedata(req.stream, buffer, ctrlcur[1], BSDIFF_WRITEEXTRA))
+			return -1;
 	};
 
 	return 0;
